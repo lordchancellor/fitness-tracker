8a552fee31d8961c395b2646dfcf594814643e27
lordchancellor <c.d.j.ford@gmail.com>
2018-06-12 23:12:02 +0100

    Added basic app structure and routing

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+const modules: any[] = [
+  MatButtonModule,
+  MatIconModule
+];
+
 @NgModule({
-  imports: [
-
-  ],
-  exports: [
-
-  ]
+  imports: modules,
+  exports: modules
 })
 export class MaterialModule {}
