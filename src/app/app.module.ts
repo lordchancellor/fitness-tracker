5cab7b9964fdc242840ee4f56ccc01445062af27
lordchancellor <c.d.j.ford@gmail.com>
2018-06-13 13:02:26 +0100

    Added very basic signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 import { MaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -30,6 +31,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    FormsModule,
     MaterialModule,
     AppRoutingModule
   ],
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 
+// Buttons & Indicators
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
+// Form Controls
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 const modules: any[] = [
   MatButtonModule,
-  MatIconModule
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
 ];
 
 @NgModule({
